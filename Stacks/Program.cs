0a1a346d1ecbb9e7cf151379b457997d0dc5dd9e
Matthew Coleman <K31073@eltham-college.org.uk>
2020-01-23 12:51:45 +0000

    Add project files.

---
+++ Stacks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack NewStack = new Stack(10);
+            string quit = "";
+            while ( quit != "Q")
+            {
+                Console.WriteLine("press Q to quit or c to continue");
+                Console.WriteLine("Do you want to add or remove");
+                string answer = Console.ReadLine();
+
+
+            }
+        //    NewStack.AddToStack();
+
+        }
+    }
+}
---
+++ Stacks/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks
+{
+    class Stack
+    {
+        private char[] stack;
+        private int bottom;
+        private int top;
+
+        public Stack (int size)
+        {
+            stack = new char[size];
+            bottom = 0;
+        }
+        public void AddToStack (char c)
+        {
+            top = top + 1;
+            top = c;
+            
+        }
+        public char RemoveFromStack()
+        {
+            if(IsEmpty(top) == true)
+            {
+                Console.WriteLine("stack already empty");
+            }
+            if (IsEmpty(top) == false)
+            {
+                
+            }
+
+            return ' ';
+        }
+
+        private static bool IsEmpty(int top)
+        {
+            if (top == 0)
+            {
+                return true;
+            }
+                return false;
+        }
+    }
+}
